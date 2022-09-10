fdfb8aa665afd9e5edbb0ac308a75454ecb1bde0
Raph4sure <raph4sure007@gmail.com>
2022-09-09 18:07:12 -0700

    program that prints the lowercase alphabet in reverse, followed by a new line.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Using main function;
+ *
+ *Description: This program print if the number is positive, zero, or negative
+ *Return: 0
+ */
+int main(void)
+{
+	char d;
+
+	for (d = 'z'; d >= 'a' ; d--)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+		return (0);
+}
