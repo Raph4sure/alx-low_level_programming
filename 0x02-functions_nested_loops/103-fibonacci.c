a8af1e3c65e079cecfa75c173e21f492d675d761
Raph4sure <raph4sure007@gmail.com>
2022-09-14 15:44:27 -0700

    program that finds and prints the sum of the even-valued terms

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - This is the entry
- * Description: write a program that finds and prints the sum of the 
+ * Description: write a program that finds and prints the sum of the
  * even-valued terms in fibonacci
  * Return: 0
  */
