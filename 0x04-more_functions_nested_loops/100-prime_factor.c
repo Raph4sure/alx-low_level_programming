2b59340238446d7191083d95f629ef70ef05d458
Raph4sure <raph4sure007@gmail.com>
2022-09-15 15:43:34 -0700

    This is solution for all more function nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,13 +8,32 @@
  */
 int main(void)
 {
-	unsigned long int i, n = 612852475143;
+	long int n;
+	long int max;
+	long int i;
 
-	for (i = 3; i < 782849; i = i + 2)
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
 	{
-		while ((n % i == 0) && (n != 1))
-			n = n / i;
+		max = 2;
+		n /= 2;
 	}
-	printf("%lu\n", n);
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
 	return (0);
 }
