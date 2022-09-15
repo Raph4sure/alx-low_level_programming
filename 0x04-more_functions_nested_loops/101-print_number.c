333520f6037ce444e037fe730b026057b2ae810f
Raph4sure <raph4sure007@gmail.com>
2022-09-15 16:02:38 -0700

    This is solution for all more function nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,7 +12,7 @@ void print_number(int n)
 	if (n < 0)
 	{
 		n1 = -n;
-		_putchar('_');
+		_putchar('-');
 	} else
 	{
 		n1 = n;
@@ -22,5 +22,6 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
+
 	_putchar((n1 % 10) + '0');
 }
