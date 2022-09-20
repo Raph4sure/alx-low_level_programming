249ede05500b0bbb714a695c2fb8289899963c85
Raph4sure <raph4sure007@gmail.com>
2022-09-19 17:29:41 -0700

    function that returns the length of a string.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
-/**
- * _puts - function to print string
+
+/** _puts - function to print string
  * Description - prints a tring, followed by a new line
  * @str: function that accepts an input saved into str
  * Return: Always 0 (Success)
