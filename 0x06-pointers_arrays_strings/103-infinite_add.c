72cbc268bc6ef84817b22306b57623497c0b1106
Raph4sure <raph4sure007@gmail.com>
2022-09-22 16:05:29 -0700

    function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,7 +6,7 @@
  * @n1: number one.
  * @n2: number two.
  * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
+ * @size_r: buffer size
  * Return: the pointer r.
  */
 
