c58e7dd91fcbc5dcc2987e10f2214bfcb0a90439
Raph4sure <raph4sure007@gmail.com>
2022-09-27 08:15:00 -0700

    Create a file that contains the password for the crackme2 executable.

---
+++ 0x07-pointers_arrays_strings/solution_to_101-crackme_password.c
@@ -0,0 +1,8 @@
+1: open the file using vi -b 101-crackme_password
+2: When u see the vi window, don't use the insert
+mode, just type :set binary and hit enter
+3: type :set noeol and hit enter
+4: use the insert mode and type abc123
+5: esc :wq hit enter
+
+Then git add . etc
