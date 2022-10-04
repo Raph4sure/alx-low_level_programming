92d847ad9770dd9031b11ec3d2b149c0dd220744
Raph4sure <raph4sure007@gmail.com>
2022-10-04 14:24:37 -0700

    creating main.c file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,14 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * str_concat - concats strings
  * @s1: string 1
  * @s2: string 2
  * Return: returns concated string
  */
-
 char *str_concat(char *s1, char *s2)
 {
 	int i, len1, len2;
@@ -22,13 +20,11 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	while (s1[i] != '\0')
 	{
 		i++;
 		len1++;
 	}
-
 	i = 0;
 
 	while (s2[i] != '\0')
@@ -36,7 +32,6 @@ char *str_concat(char *s1, char *s2)
 		i++;
 		len2++;
 	}
-
 	conc = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (conc == NULL)
 		return (NULL);
@@ -45,7 +40,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		conc[i] = s1[i];
 	}
-
 	for (i = 0; i < len2; i++)
 	{
 		conc[i + len1] = s2[i];
