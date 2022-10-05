6f0f5ba24f23c69296ddfa595104bcb1b0d1c8d0
Raph4sure <raph4sure007@gmail.com>
2022-10-04 17:02:59 -0700

    fixing advanced task error

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,14 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * argstostr - gives array of args
  * @ac: argument count
  * @av: array of arguments
  * Return: char value
  */
-
 char *argstostr(int ac, char **av)
 {
 	int size;
@@ -30,11 +28,10 @@ char *argstostr(int ac, char **av)
 	s = malloc(sizeof(char) * size);
 	if (s == NULL)
 		return (NULL);
-
 	k = 0;
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0;  av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j] != '\0'; j++)
 		{
 			s[k] = av[i][j];
 			k++;
